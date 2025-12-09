49fb610f8c5e97f5a6820012c2bab6d2f660e951
Your Name <you@example.com>
2025-12-09 13:08:29 +0100

    code

---
+++ py/app.py
@@ -0,0 +1,45 @@
+import sys
+import subprocess as sp
+
+def getStdIn():
+	return [l for l in sys.stdin]
+
+def getModuleNames(inTxt):
+	inTxt = [l for l in inTxt if "Cannot find module" in l]
+	modNames = []
+	for l in inTxt:
+		parts = l.split(' ')
+		name = parts[parts.index('module') + 1][1:-1]
+		modNames.append(name)
+	return modNames
+
+def useErrMsgs():
+	inTxt = getStdIn()
+	return getModuleNames(inTxt)
+
+def getDepCheckNames(inTxt):
+	names = [l for l in inTxt if l[0] == '*']
+	names = [l[2:l.index(':')] for l in names]
+	return names
+
+def useDepCheck():
+	inTxt = getStdIn()
+	return getDepCheckNames(inTxt)
+
+def installPackages(names):
+	if names == []: return
+	print(names)
+	for name in names:
+		sp.run(f"npm install {name}", shell=True)
+
+def main():
+	av = sys.argv
+	ac = len(av)
+	if ac == 2:
+		if av[1] == "depcheck": names = useDepCheck()
+		elif av[1] == "errormsgs": names = useErrMsgs()
+		else: print("First arg must be depcheck or errormsgs"); exit()
+	else: print("Usage: tsc | python app.py errormsgs"); exit()
+	installPackages(names)
+if __name__ == "__main__":
+	main()
